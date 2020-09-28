b1dd17697590f146ab14e3b556bb9c9ac7014a3a
小笠原京也 <kyoya@KYOYAs-MBP.local>
2020-09-28 16:59:35 +0900

    changed axios to Axios

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -17,11 +17,10 @@ class ClientArea {
   }
 
   sendRequest() {
-    axios
-      .post(
-        'https://brave-banach-231383.netlify.app/.netlify/functions/secret-area',
-        { password: this.field.value }
-      )
+    Axios.post(
+      'https://brave-banach-231383.netlify.app/.netlify/functions/secret-area',
+      { password: this.field.value }
+    )
       .then((response) => {
         this.form.remove();
         this.contentArea.innerHTML = response.data;
